3a509e3d4bafec94815f765ced0908aa79440922
PythonFabi <fabianmelone.fw@gmail.com>
2024-01-29 17:54:16 +0100

    create apis to post and update chats based on their id

--- controllers/api/chatRoutes.js
+++ controllers/api/chatRoutes.js
@@ -4,3 +4,36 @@ const { Chat } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // create a new chat, once logged in
+router.post('/', withAuth, async (req, res) => {
+    try {
+        // create a new chat with current sessions user_id
+        const newChat = await Chat.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        // create new Blog, if status code is 200
+        res.status(200).json(newChat);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+// update chat based on id
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const updatedChat = await Chat.update(
+            {
+                ...req.body
+            },
+            {
+                where: {
+                    id: req.params.id,
+                    user_id: req.session.user_id,
+                },
+            } );
+            res.status(200).json(updatedChat);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
